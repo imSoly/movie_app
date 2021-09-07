69b8e64513b75643de447d684e2c92b8cd6e1e1f
imSoly <ohimsoly@gmail.com>
2021-09-08 01:37:26 +0900

    Rendering the movies. Make Movie function Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 // import PropTypes from "prop-types";
 
 class App extends React.Component {
@@ -13,7 +14,9 @@ class App extends React.Component {
       data: {
         data: { movies },
       },
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
     this.setState({ movies, isLoading: false });
   };
 
@@ -22,8 +25,23 @@ class App extends React.Component {
   }
 
   render() {
-    const isLoading = this.state.isLoading;
-    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading"
+          : movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
   }
 }
 
