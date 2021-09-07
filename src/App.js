4438a5dc4f96d7d99411fcbaa7fdca6570dcfe7e
imSoly <ohimsoly@gmail.com>
2021-09-08 00:33:13 +0900

    Screen loading message

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
 
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
 
   render() {
-    return (
-      <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>ADD</button>
-        <button onClick={this.minus}>MINUS</button>
-      </div>
-    );
+    const isLoading = this.state.isLoading;
+    console.log(isLoading);
+    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
   }
 }
 
